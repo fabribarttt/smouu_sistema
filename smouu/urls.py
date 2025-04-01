4fcb2b50ef3006e70123c36bf1b5ceb46e4770bd
Fabricio Bartomeu <86902293+fabribarttt@users.noreply.github.com>
2025-04-01 07:58:26 -0300

    crud clients

--- smouu/urls.py
+++ smouu/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Login, Panel principal, Logout
     path("", views.login_view, name="login"),
     path("home/", views.home_view, name="home"),
     path("logout/", views.logout_view, name="logout"),
-    path("register_client/", views.register_client_view, name="register_client"),
+    # CRUD Clientes
+    path("create_client/", views.create_client_view, name="create_client"),
+    path("read_client/", views.read_client_view, name="read_client"),
+    path("update_client/<int:pk>", views.update_client_view, name="update_client"),
+    path("delete_client/<int:pk>", views.delete_client_view, name="delete_client"),
+    # CRUD Dispositivos
 ]
--- smouu/views.py
+++ smouu/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import RegisterClientForm
+from .models import Cliente
 
 
 def login_view(request):
@@ -33,17 +34,53 @@ def home_view(request):
     return render(request, "home.html")
 
 
+# CRUD CLIENTES
 # Funcion para registrar los clientes renderiza el html y recibe un POST con los datos ingresados en el formulario y lo guarda en la base de datos
-def register_client_view(request):
+def create_client_view(request):
     if request.user.is_authenticated:
         form = RegisterClientForm(request.POST or None)
         if request.method == "POST":
             if form.is_valid():
                 form.save()
                 messages.success(request, "El cliente se ha registrado correctamente")
-                return redirect("home")
+                return redirect("read_client")
         else:
-            return render(request, "register_client.html", {"form": form})
+            return render(request, "clients/create_client.html", {"form": form})
     else:
         messages.error(request, "Debes de iniciar sesion")
         return redirect("login")
+
+
+# Funcion para visualizar los clientes ya registrados y renderiza el HTML
+def read_client_view(request):
+    clientes = Cliente.objects.all
+
+    return render(request, "clients/read_client.html", {"clientes": clientes})
+
+
+def update_client_view(request, pk):
+    if request.user.is_authenticated:
+        current_client = Cliente.objects.get(id=pk)
+        form = RegisterClientForm(request.POST or None, instance=current_client)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Los datos del cliente se actualizaron")
+            return redirect("read_client")
+        return render(request, "clients/update_client.html", {"form": form})
+    else:
+        messages.error(request, "Debes de iniciar sesion")
+        return redirect("login")
+
+
+def delete_client_view(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Cliente.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Se ha eliminado correctamente")
+        return redirect("read_client")
+    else:
+        messages.error(request, "Debes iniciar sesion")
+        return redirect("login")
+
+
+# CRUD Dispositivos
